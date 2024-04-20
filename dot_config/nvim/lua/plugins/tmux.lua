7db53877045faf9a889f2490835ac80f56405dd7
Dr. Sascha Gerloff <sascha.gerloff@talentwunder.com>
2024-04-20 13:27:09 +0200

    add keymaps to tmux integration

--- dot_config/nvim/lua/plugins/tmux.lua
+++ dot_config/nvim/lua/plugins/tmux.lua
@@ -1,3 +1,7 @@
 return {
   { "christoomey/vim-tmux-navigator" },
+  vim.keymap.set("n", "<c-k>", ":TmuxNavigateDown<CR>"),
+  vim.keymap.set("n", "<c-j>", ":TmuxNavigateUp<CR>"),
+  vim.keymap.set("n", "<c-h>", ":TmuxNavigateLeft<CR>"),
+  vim.keymap.set("n", "<c-l>", ":TmuxNavigateRight<CR>"),
 }
