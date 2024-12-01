d0a6e5d0ce40f2cc472b984c82b1af2f1fda2458
Dr. Sascha Gerloff <sascha.gerloff@talentwunder.com>
2024-12-01 20:12:38 +0100

    simplify nvim config to prepare for usage with vscode

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,5 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- Isolated python environment for neovim plugins
-vim.g.python3_host_prog = "~/.local/share/nvim_venv/bin/python"
--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,25 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local treesitter_keymaps = {
+  { key = "<leader>ta", motion = "goto_next_start", textobj = "@parameter.inner" },
+  { key = "<leader>tA", motion = "goto_next_end", textobj = "@parameter.inner" },
+  { key = "<leader>Ta", motion = "goto_previous_start", textobj = "@parameter.inner" },
+  { key = "<leader>TA", motion = "goto_previous_end", textobj = "@parameter.inner" },
+  { key = "<leader>tf", motion = "goto_next_start", textobj = "@function.outer" },
+  { key = "<leader>tF", motion = "goto_next_end", textobj = "@function.outer" },
+  { key = "<leader>Tf", motion = "goto_previous_start", textobj = "@function.outer" },
+  { key = "<leader>TF", motion = "goto_previous_end", textobj = "@function.outer" },
+}
+
+for _, map in ipairs(treesitter_keymaps) do
+  vim.keymap.set(
+    "n",
+    map.key,
+    require("config.utils").dot_repeat(function()
+      require("nvim-treesitter.textobjects.move")[map.motion](map.textobj)
+    end),
+    { expr = true }
+  )
+end
