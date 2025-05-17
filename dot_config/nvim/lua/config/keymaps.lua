d80bab18703655adea6bedb28af19ef73699c00b
Dr. Sascha Gerloff <sascha.gerloff@talentwunder.com>
2025-05-17 12:44:39 +0200

    remove test for passing environment variables to ipython shell

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -3,13 +3,13 @@
 -- Add any additional keymaps here
 
 -- Lowercase global marks
-local low = function(i) return string.char(97+i) end
-local upp = function(i) return string.char(65+i) end
+local low = function(i) return string.char(97 + i) end
+local upp = function(i) return string.char(65 + i) end
 
-for i=0,25 do vim.keymap.set("n", "m"..low(i), "m"..upp(i)) end
-for i=0,25 do vim.keymap.set("n", "m"..upp(i), "m"..low(i)) end
-for i=0,25 do vim.keymap.set("n", "'"..low(i), "'"..upp(i)) end
-for i=0,25 do vim.keymap.set("n", "'"..upp(i), "'"..low(i)) end
+for i = 0, 25 do vim.keymap.set("n", "m" .. low(i), "m" .. upp(i)) end
+for i = 0, 25 do vim.keymap.set("n", "m" .. upp(i), "m" .. low(i)) end
+for i = 0, 25 do vim.keymap.set("n", "'" .. low(i), "'" .. upp(i)) end
+for i = 0, 25 do vim.keymap.set("n", "'" .. upp(i), "'" .. low(i)) end
 
 -- Execute LUA
 vim.keymap.set("n", "<leader><leader>x", "<cmd>source %<CR>")
