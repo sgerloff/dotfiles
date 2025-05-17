a73c97a9ef9483fb0a9d1f5e257dae017dd3b8b2
Dr. Sascha Gerloff <sascha.gerloff@talentwunder.com>
2025-05-17 13:11:12 +0200

    add better hotkeys to ipython shell

---
+++ dot_ipython/profile_default/ipython_config.py
@@ -0,0 +1,17 @@
+c.TerminalInteractiveShell.shortcuts = [
+    {
+        "command": "IPython:auto_suggest.accept_or_jump_to_end",
+        "new_keys": ["c-l"],
+        "create": True,
+    },
+    {
+        "command": "IPython:auto_suggest.down_and_update_hint",
+        "new_keys": ["c-j"],
+        "create": True,
+    },
+    {
+        "command": "IPython:auto_suggest.up_and_update_hint",
+        "new_keys": ["c-k"],
+        "create": True,
+    },
+]
