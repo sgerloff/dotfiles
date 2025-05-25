d30ccb4c5688d150f6c43a0bb889024821f7077b
Dr. Sascha Gerloff <sascha.gerloff@talentwunder.com>
2025-05-25 21:47:59 +0200

    better help pages

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -5,21 +5,35 @@ return {
     enabled = true,
     build = ":TSUpdate",
     config = function()
-      require'nvim-treesitter.configs'.setup {
-	ensure_installed = { "lua", "markdown", "markdown_inline" },
-	sync_install = false,
-	auto_install = true,
-	highlight = {
-	  enable = true,
-	  disable = function(lang, buf)
-	      local max_filesize = 1000 * 1024 -- 1000 KB
-	      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-	      if ok and stats and stats.size > max_filesize then
-		  return true
-	      end
-	  end,
-	  additional_vim_regex_highlighting = false,
-	},
+      require 'nvim-treesitter.configs'.setup {
+        ensure_installed = {
+          "vim",
+          "lua",
+          "markdown",
+          "markdown_inline",
+          "python",
+          "vimdoc",
+          "just",
+          "bash",
+          "json",
+          "mermaid",
+          "csv",
+          "tsv",
+          "yaml",
+        },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          disable = function(lang, buf)
+            local max_filesize = 1000 * 1024 -- 1000 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+          additional_vim_regex_highlighting = false,
+        },
       }
     end
   }
